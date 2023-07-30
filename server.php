1d30d4ef85a20e7b2bdf10ea0fda1040e0011be4
Astroidworld <astroidworld@gmail.com>
2023-07-30 23:07:27 +0530

    server.php command line removed

--- server.php
+++ server.php
@@ -1,31 +1,5 @@
 <?php
 
-    // $code = $_POST['code'];
-    // $input = $_POST['input'];
-    // //input save in file
-    // $inputfilepath = "program_files/input.txt";
-    // $inputfile = fopen($inputfilepath, "w");
-    // fwrite($inputfile, $input);
-    // fclose($inputfile);
-
-    // //code data save in file
-    // $filename = substr(md5(mt_rand()), 0, 5);
-    // $codepath = "program_files/" . $filename . "." . "js"; 
-    // $codefile = fopen($codepath, "w");
-    // fwrite($codefile, $code);
-    // fclose($codefile);
-
-    // $commond = "node $codepath 2>&1" . "<" ."program_files/input.txt";
-    // // shell_exec($commond);
-    // // $commond = "hello.exe";
-    // $output = shell_exec($commond);
-
-    // unlink($codepath);
-
-    // echo $output;
-
-
-
     $code = $_POST['code'];
     $input = $_POST['input'];
     $lang = $_POST['lang'];
@@ -55,7 +29,7 @@
     $output = shell_exec($command);
 
     unlink($codepath);
-    
+
     if ($lang == "c_c++") {        
         unlink("csourcecode.exe");
     } 
